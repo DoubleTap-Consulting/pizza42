eb1786267fb59964426b264c3745973504166ce9
michaelaflores <mike@michaelflores.io>
2017-11-02 21:44:03 -0700

    Address #2
    Created the MuiTheme and set up various settings in accordance with the color theme we generated.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Home from './containers/home/Home';
 import Login from './containers/login/Login';
 import NavBar from 'containers/navBar/NavBar';
+import muiTheme from 'shared-components/muiTheme';
+import { MuiThemeProvider } from 'material-ui/styles';
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
@@ -8,13 +10,15 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavBar />
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/" render={() => <div>Default fallback test</div>} />
-        </Switch>
-      </div>
+      <MuiThemeProvider theme={muiTheme}>
+        <div className="App">
+          <NavBar />
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/" render={() => <div>Default fallback test</div>} />
+          </Switch>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
