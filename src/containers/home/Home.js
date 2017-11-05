19f26dff99dee4a9bc76803a3a6379370d34b21d
michaelaflores <mike@michaelflores.io>
2017-11-04 22:18:41 -0700

    Add and PizzaCard component and layout to Home container

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,4 +1,19 @@
+import Grid from 'material-ui/Grid';
+import PizzaCard from 'components/pizzaCard/PizzaCard';
 import React, { Component } from 'react';
+import { withStyles } from 'material-ui/styles';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    marginTop: 30,
+  },
+  paper: {
+    padding: 16,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
 
 export class Home extends Component {
   constructor(props) {
@@ -7,10 +22,32 @@ export class Home extends Component {
   }
 
   render() {
+    const { classes } = this.props;
     return (
-      <div></div>
+      <div className={classes.root}>
+        <Grid container spacing={24}>
+          <Grid item xs={12} sm={6}>
+            <PizzaCard />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <PizzaCard />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <PizzaCard />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <PizzaCard />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <PizzaCard />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <PizzaCard />
+          </Grid>
+        </Grid>
+      </div>
     );
   }
 }
 
-export default Home;
+export default withStyles(styles)(Home);
