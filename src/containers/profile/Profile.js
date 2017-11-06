f30f3a7694f38311282d0b800080e25cc460726f
michaelaflores <mike@michaelflores.io>
2017-11-05 23:25:23 -0800

    Add a potential look for profile header

--- src/containers/profile/Profile.js
+++ src/containers/profile/Profile.js
@@ -1,17 +1,71 @@
+import Avatar from 'material-ui/Avatar';
+import Auth from 'utils/auth';
+import Card, { CardHeader, CardMedia, CardContent, CardActions } from 'material-ui/Card';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { withStyles } from 'material-ui/styles';
+
+const styles = theme => ({
+  card: {
+    // maxWidth: 400,
+  },
+  media: {
+    height: 194,
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: '#F44336',
+  },
+  flexGrow: {
+    flex: '1 1 auto',
+  },
+});
 
 export class Profile extends Component {
+  static propTypes = {
+    classes: PropTypes.object.isRequired
+  }
+
   constructor(props) {
     super(props);
     this.state = {};
+    this.auth = new Auth();
+  }
+
+  componentWillMount() {
+    this.setState({ profile: {} });
+    this.auth.getProfile((err, profile) => {
+      this.setState({ profile }, () => {
+        console.log('profile state:', this.state);
+      });
+    });
   }
 
   render() {
+    const { classes } = this.props;
+    const { profile } = this.state;
     return (
-      <div></div>
+      <Card className={classes.card}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="Recipe" className={classes.avatar}>
+              M
+          </Avatar>
+          }
+          title="mike@michaelflores.io"
+          subheader="Member since November 5th, 2017"
+        />
+      </Card>
     );
   }
 }
 
-export default Profile;
+export default withStyles(styles)(Profile);
