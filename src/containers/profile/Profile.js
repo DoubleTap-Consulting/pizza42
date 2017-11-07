7375585a72ee403659bdb6a9a79e9e01984a6339
michaelaflores <mike@michaelflores.io>
2017-11-06 20:48:37 -0800

    Add social logos, update social profile card ui

--- src/containers/profile/Profile.js
+++ src/containers/profile/Profile.js
@@ -1,9 +1,14 @@
 import Avatar from 'material-ui/Avatar';
 import Auth from 'utils/auth';
+import Button from 'material-ui/Button';
 import callApi from 'utils/api';
 import Card, { CardHeader, CardMedia, CardContent, CardActions } from 'material-ui/Card';
+import emailIcon from 'images/icon_email.png';
+import googleLogo from 'images/logo_google.png';
 import Grid from 'material-ui/Grid';
+import facebookLogo from 'images/logo_facebook.jpg';
 import PropTypes from 'prop-types';
+import twitterLogo from 'images/logo_twitter.png';
 import React, { Component } from 'react';
 import Typography from 'material-ui/Typography';
 import { withStyles } from 'material-ui/styles';
@@ -48,6 +53,9 @@ export class Profile extends Component {
       }
     };
     this.auth = new Auth();
+    this.logos = {
+      facebookLogo,
+    };
   }
 
   componentWillMount() {
@@ -84,20 +92,94 @@ export class Profile extends Component {
           />
         </Card>
         <Grid container justify="center" spacing={24}>
-          {this.state.profile.user_metadata.socialProfiles.map((account, index) => (
-            <Grid key={account.typeName} item xs={6}>
-              <Card>
-                <CardContent>
-                  <Typography type="headline">
-                    {account.typeName}
+          <Grid item xs={6}>
+            <Card>
+              <CardMedia
+                image={facebookLogo}
+                style={{ height: 200 }}
+                title="Facebook logo"
+              />
+              <CardContent>
+                <Typography type="headline">
+                  Facebook
+                </Typography>
+                <Typography type="body1">
+                  Body text
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button dense color="primary">
+                  Link Account
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={6}>
+            <Card>
+              <CardMedia
+                image={twitterLogo}
+                style={{ height: 200 }}
+                title="Twitter logo"
+              />
+              <CardContent>
+                <Typography type="headline">
+                  Twitter
                   </Typography>
-                  <Typography type="body1">
-                    {account.url}
+                <Typography type="body1">
+                  Body text
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button dense color="primary">
+                  Link Account
+              </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={6}>
+            <Card>
+              <CardMedia
+                image={googleLogo}
+                style={{ height: 200 }}
+                title="Google logo"
+              />
+              <CardContent>
+                <Typography type="headline">
+                  Google
                   </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
+                <Typography type="body1">
+                  Body text
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button dense color="primary">
+                  Link Account
+              </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={6}>
+            <Card>
+              <CardMedia
+                image={emailIcon}
+                style={{ height: 200 }}
+                title="Email logo"
+              />
+              <CardContent>
+                <Typography type="headline">
+                  Email
+                  </Typography>
+                <Typography type="body1">
+                  Body text
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button dense color="primary">
+                  Link Account
+              </Button>
+              </CardActions>
+            </Card>
+          </Grid>
         </Grid>
       </div>
     );
