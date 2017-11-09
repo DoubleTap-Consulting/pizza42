b9d6b5a60a5ef32b25044ed1b40e6d05d7afe53f
Michael Mitrakos <mike.mitrakos@gmail.com>
2017-11-08 22:52:04 -0800

    [fix]

--- src/containers/profile/Profile.js
+++ src/containers/profile/Profile.js
@@ -51,6 +51,7 @@ export class Profile extends Component {
     this.state = {
       profile: {
         name: '',
+        sub: '',
         user_metadata: {
           socialProfiles: [],
         },
