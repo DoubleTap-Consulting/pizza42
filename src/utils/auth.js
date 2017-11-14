479914d6d06feea3e4e76824947df3f22f2aa894
Michael Mitrakos <mike.mitrakos@gmail.com>
2017-11-14 01:24:46 +0100

    [update] update prod url

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -5,7 +5,7 @@ export default class Auth {
   auth0 = new auth0.WebAuth({
     domain: 'doubletap-consulting.auth0.com',
     clientID: 'rTPIMjZe2grcF0R1Wivk3reWoQoQakO9',
-    redirectUri: 'http://localhost:3000/callback',
+    redirectUri: 'http://ec2-54-183-30-7.us-west-1.compute.amazonaws.com/callback',
     audience: 'https://doubletap-consulting.auth0.com/userinfo',
     responseType: 'token id_token',
     scope: 'openid profile',
